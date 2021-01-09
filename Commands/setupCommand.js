60e4139421465710b7a23fdad69718e67e627b23
Paradise Bots <67729779+ParadiseBotList@users.noreply.github.com>
2021-01-08 21:58:46 -0700

    Update setupCommand.js

--- Commands/setupCommand.js
+++ Commands/setupCommand.js
@@ -64,7 +64,7 @@ module.exports.help = {
 module.exports.requirements = {
     userPerms: [],
     clientPerms: ["EMBED_LINKS"],
-    devOnly: true
+    devOnly: false
 }
 
 module.exports.limits = {
