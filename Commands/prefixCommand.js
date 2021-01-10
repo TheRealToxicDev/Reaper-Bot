ad1e3e1eb958aec00a3b0d7788ff68366b0e3a5b
Toxic Dev <59587139+TheRealToxicDev@users.noreply.github.com>
2021-01-10 02:32:42 -0700

    Update prefixCommand.js

--- Commands/prefixCommand.js
+++ Commands/prefixCommand.js
@@ -21,7 +21,7 @@ module.exports.run = async (client, message, args) => {
        .setTimestamp()
        .setFooter(EmbedComponents.embedFooter, EmbedComponents.embedImage)
 
-    if (!args[0]) return message.channel.send(tomany_args);
+    if (args[0].length > 10) return message.channel.send(tomany_args);
     
         let missing_args = new MessageEmbed()
        .setAuthor('Error: Missing Args', EmbedComponents.embedImage)
@@ -30,8 +30,8 @@ module.exports.run = async (client, message, args) => {
        .addField('Current Prefix', 'tox.', true)
        .setTimestamp()
        .setFooter(EmbedComponents.embedFooter, EmbedComponents.embedImage)
-
-    if (args[0].length > 10) return message.channel.send(missing_args);
+    
+    if (!args[0]) return message.channel.send(missing_args);
 
     guild.prefix = args[0]
 
