5bdbeae36352165e454adb52ba86f8d3740a0582
Toxic Dev <59587139+TheRealToxicDev@users.noreply.github.com>
2021-01-09 21:41:01 -0700

    Update serverStatsCommand.js

--- Commands/serverStatsCommand.js
+++ Commands/serverStatsCommand.js
@@ -25,8 +25,6 @@ module.exports.run = async (client, message, args) => {
                 let result = [];
     
                 let index = 1;
-                
-                if (players < 1 || players.includes('[]')) players = '0';
     
                 for (let player of server.players) {
     
@@ -34,13 +32,14 @@ module.exports.run = async (client, message, args) => {
                 }
                 
                 if (result < 1) result = 'No Players Online'
+                
     
                 const result_embed = new MessageEmbed()
                    .setAuthor('Server Information', EmbedComponents.embedImage)
                    .setColor(EmbedColors.onlineColor)
                    .setDescription('**STATUS:** ONLINE 🟢')
                    //.addField('Player Count', `${server.players.length}/${server.infos.vars.sv_maxClients}`, true)
-                   //.addField('Player List', result, true)
+                   .addField('Player List', result, true)
                    .setTimestamp()
                    .setFooter(EmbedComponents.embedFooter, EmbedComponents.embedImage)
     
