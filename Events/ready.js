25d13357edbf1ea7758faf8136b2c8ccfabc4696
Toxic Dev <59587139+TheRealToxicDev@users.noreply.github.com>
2021-02-14 20:15:59 -0700

    Update ready.js

--- Events/ready.js
+++ Events/ready.js
@@ -4,14 +4,49 @@ const { join } = require("path")
 const filePath2 = join(__dirname, "..", "Events");
 const eventFiles2 = readdirSync(filePath2);
 const timers = require("timers");
-const HttpRequest = require('request');
+const fetch = require ('node-fetch');
 const { MessageEmbed } = require ('discord.js');
 const EmbedColors = require ('@Embeds/colors');
 const EmbedComponents = require('@Embeds/components');
 
-module.exports = async (client) => {
+const package = require('../package.json');
 
-  CheckForUpdate();
+module.exports = async (client) => {
+  
+  const ready_channel = client.channels.cache.find(c => c.id === process.env.READY_LOGS);
+  
+   await fetch(process.env.GITHUB_LINK)
+      .then(res => res.json())
+      .then(json => {
+
+            if (json.NewestVersion === package.version) {
+              
+            let up_to_date = new MessageEmbed()
+            .setAuthor('Version Check Initiated', EmbedComponents.embedImage)
+            .setColor(EmbedColors.onlineColor)
+            .setDescription('Up-To Date and Ready to go!!')
+            .addField('Current Version', `${package.version}`)
+            .addField('Newest Version', `${json.NewestVersion}`)
+            .setTimestamp()
+            .setFooter(EmbedComponents.embedFooter, EmbedComponents.embedImage)
+              
+            return ready_channel.send(up_to_date);
+              
+            } else if (json.NewestVersion !== package.version) { {
+                let outdated = new MessageEmbed()
+                .setAuthor('Version Check: Failed', EmbedComponents.embedImage)
+                .setColor(EmbedColors.onlineColor)
+                .setDescription('You are using an Outdated version of [FiveM Stats Bot](https://statsbot.toxicdev.me/), Please download the Newest Release to avoid any issues.')
+                .addField('Current Version', `${package.version}`)
+                .addField('Newest Version', `${json.NewestVersion}`)
+                .addField('Change Log', `${json.ChangeLog}`)
+                .setTimestamp()
+                .setFooter(EmbedComponents.embedFooter, EmbedComponents.embedImage)
+
+                     return ready_channel.send(outdated)
+                  }
+            }
+      })
 
 let activities = [
     {
@@ -51,68 +86,4 @@ let activities = [
 
     i++;
   }, 30000);
-  
-  
-  function CheckForUpdate() {
-    
-    const ready_channel = client.channels.cache.find(c => c.id === process.env.READY_LOGS);
-
-    const current_version = '1.0.0';
-    const GithubResourceName = 'FiveM-Stats-Bot';
-
-    HttpRequest(
-        {
-            url: `https://raw.githubusercontent.com/TheRealToxicDev/${process.env.RESOURCE_REPO}/master/${process.env.PROJECT_NAME}/VERSION`,
-            timeout: 5000,
-            time: true
-        },
-
-        function (Error, Response, NewestVersion) {
-
-            let version_check = new MessageEmbed()
-            .setAuthor('Version Check Initiated', EmbedComponents.embedImage)
-            .setColor(EmbedColors.onlineColor)
-            .setDescription('Comparing the Current Version to Newest Release')
-            .addField('Current Version', `${current_version}`)
-            .addField('Newest Version', `${NewestVersion}`)
-            .setTimestamp()
-            .setFooter(EmbedComponents.embedFooter, EmbedComponents.embedImage)
-
-            ready_channel.send(version_check)
-
-            if (NewestVersion == current_version) {
-                let up_to_date = new MessageEmbed()
-                .setAuthor('Version Check: Status', EmbedComponents.embedImage)
-                .setColor(EmbedColors.onlineColor)
-                .setDescription('The Version Check was Successful, Enjoy!!')
-                .setTimestamp()
-                .setFooter(EmbedComponents.embedFooter, EmbedComponents.embedImage)
-                
-                ready_channel.send(up_to_date)
-            } else {
-                let outdated = new MessageEmbed()
-                outdated.setAuthor('Version Check: Failed', EmbedComponents.embedImage)
-                outdated.setColor(EmbedColors.onlineColor)
-                outdated.setDescription('You are using an Outdated version of [FiveM Stats Bot](https://statsbot.toxicdev.me/), Please download the Newest Release to avoid any issues.')
-                outdated.addField('Current Version', `${current_version}`)
-                outdated.addField('Newest Version', `${NewestVersion}`)
-
-                HttpRequest(
-                    {
-                        url: `https://raw.githubusercontent.com/TheRealToxicDev/${process.env.RESOURCE_REPO}/master/${process.env.PROJECT_NAME}/CHANGES`,
-                        timeout: 5000,
-                        time: true
-                    },
-                    function (Error, Response, Changes) {
-                        outdated.addField('Change Log', `${Changes}`)
-                        outdated.setTimestamp()
-                        outdated.setFooter(EmbedComponents.embedFooter, EmbedComponents.embedImage)
-                        
-                        ready_channel.send(outdated)
-                    }
-                )
-            }
-        }
-    )
-}
 }
